447c519ac905bc083216e7d7c207c74b4c48c28c
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-17 12:51:56 +0800

    added custom user profile, manager and migrations

--- user-managment-REST/profiles_api/models.py
+++ user-managment-REST/profiles_api/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
-# Create your models here.
+#for userprofile
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
+#for custom userprofile manage
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profile"""
+    
+    def create_user(self, email, name, password=None):
+        """create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+
+        #best practice to use (using=self.db)
+        user.save(using=self._db)
+
+        return user
+
+
+    def create_superuser(self, email, name, password):
+        """create a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        #automatically created by PermissionsMixin
+        user.is_superuser = True
+        
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    object = UserProfileManager()
+
+    #overwrite default username to use email, so that Admin portal will display email and password
+    #instad of username and password
+    #also mark email as required or mandatory field
+    USERNAME_FIELD = 'email'
+
+    #explicitly declar fields which are required
+    REQUIRED_FIELDS =['name']
+
+    def get_full_name(self):
+        """retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """retrieve short name of user"""
+        return self.name
+
+    def __str__(self):
+        """return string representation of user"""
+        return self.email
--- user-managment-REST/profiles_project/settings.py
+++ user-managment-REST/profiles_project/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# register custom user profile model as AUTH_USER_MODEL
+#'app_name.ModelName'
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
