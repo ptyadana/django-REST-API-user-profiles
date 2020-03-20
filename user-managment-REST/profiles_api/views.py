a2e7b5372b3d2d83ab7102d2c79d3bb29255ec09
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-20 12:41:08 +0800

    add filter for searching

--- user-managment-REST/profiles_api/views.py
+++ user-managment-REST/profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
@@ -114,5 +115,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
 
 
