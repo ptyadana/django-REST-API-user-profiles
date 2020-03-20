443a173a90ccf9c6ef073eeeb4445bc2b2f96064
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-20 13:17:53 +0800

    add Login API and view

--- user-managment-REST/profiles_api/views.py
+++ user-managment-REST/profiles_api/views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -119,4 +121,11 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
 
+
+#overwrite the default Login classes to display on Browser 
+#so that we can test via Browser
+#normally login module is not visible, now we attached this to View
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
